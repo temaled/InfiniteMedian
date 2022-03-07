e971850626b38d0b91b7f1c841087bee1f7d3791
temaled <temaledyisfa@gmail.com>
2022-03-07 10:08:21 +0300

    java doc and comments for the CustomHashMap, also custom hashmap used, forgot to use it the first time

--- src/main/java/com/example/InfiniteMedian/client/Globals.java
+++ src/main/java/com/example/InfiniteMedian/client/Globals.java
@@ -10,7 +10,7 @@ public class Globals {
     /**
      * Holds the median data for all the supported symbols
      */
-    public static volatile CustomHashMap<String, SymbolData> medianData = new CustomHashMap<>();
+    public static CustomHashMap<String, SymbolData> medianData = new CustomHashMap<>();
 
     /**
      * Supported symbols by Binance, in turn by the App.
@@ -19,7 +19,7 @@ public class Globals {
     public static JSONArray supportedSymbols;
 
     /**
-     * Gets the up tod ate info about a particular info
+     * Gets the up-to-date info about a particular symbol
      * @param symbolName the name of the symbol from Binance trade data
      * @return a JSON object containing the symbol name, the median, the latest price
      * and number of data used calculating the median so far
@@ -50,7 +50,7 @@ public class Globals {
         JSONObject newData = new JSONObject(message).getJSONObject("data");
         String symbolName = newData.get("s").toString().toLowerCase();
         if (Globals.medianData.containsKey(symbolName)){
-            // updating info about a particular info
+            // updating info about a particular symbol
             // this will be executed when the 2nd or later data arrives
             SymbolData data =  Globals.medianData.get(symbolName);
             double newPrice = newData.getDouble("p");
@@ -60,6 +60,17 @@ public class Globals {
                 data.highestPrice = newPrice;
             }
             data.latestPrice = newPrice;
+            /**
+             * The calculation performed below is THE BEST for this specific case because the data
+             * fluctuates up and down very little over longer period of time. And it can perform
+             * Trillions or even more operations in log(1) time constant and without any memory overhead
+             *
+             * The only case this simple but very efficient algorithm might fail to predict the median
+             * with the required tolerance is if there's a price surge or plummet, which occurs rarely,
+             * and even then, the median accuracy will be corrected eventually.
+             *
+             * Also, the limit as time approaches infinity is the perfect median of the values.
+             */
             data.median = (data.highestPrice + data.lowestPrice)/2;
             data.numberOfData++;
 
--- src/main/java/com/example/InfiniteMedian/helpers/CustomHashMap.java
+++ src/main/java/com/example/InfiniteMedian/helpers/CustomHashMap.java
@@ -1,16 +1,42 @@
 package com.example.InfiniteMedian.helpers;
 
+/**
+ * Custom HashMap implementation
+ * @param <K> Type of the key
+ * @param <V> Type of the value
+ */
 public class CustomHashMap<K, V>{
 
     private Entry<K, V> table[];
+    private int size;
 
     private final int SIZE = 2048;
+
+
+    /**
+     * Instantiates new {@link CustomHashMap} with {@link CustomHashMap#size} zero
+     * and table size of {@link CustomHashMap#SIZE}
+     */
     public CustomHashMap(){
-        table = new Entry[SIZE];
+        this.table = new Entry[SIZE];
+        this.size = 0;
     }
 
-    private int getHash(K key){
-        String _key = key.toString();
+    /**
+     * Generates a perfect hash for the possible symbols from
+     * Binance crypto trade platform
+     * @param key The string to hash, The exchange trade symbol name for our case
+     * @return A perfect hash for the key (symbol)
+     */
+    public int getHash(K key){
+        String _key = key.toString().toLowerCase();
+        /**
+         * Prime number are chosen because one can not be a factor of another,
+         * and these specific primes are chosen because sum of any multiples of any
+         * combinations of the numbers in the list does not result in integer multiples
+         * of the others, which makes the hash function perfect when the calculation below
+         * is performed
+         */
         int[] specialPrimesList = {41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
 
         int hash = 0;
@@ -20,6 +46,19 @@ public class CustomHashMap<K, V>{
         return hash;
     }
 
+    /**
+     * Get size of the hashmap
+     * @return size of the hashmap
+     */
+    public int size(){
+        return  size;
+    }
+
+    /**
+     * Puts new entry to the HashMap
+     * @param key key of the entry
+     * @param value value to put
+     */
     public void put(K key, V value){
         int hash = getHash(key) % SIZE;
         Entry<K, V> entry = table[hash];
@@ -27,21 +66,28 @@ public class CustomHashMap<K, V>{
             table[hash] = new Entry<>(key, value);
         }else{
             while(entry.next != null){
-                if (entry.getKey() == key){
+                if (entry.getKey().equals(key)){
                     entry.setValue(value);
                     return;
                 }
                 entry = entry.next;
             }
 
-            if (entry.getKey() == key){
+            if (entry.getKey().equals(key)){
                 entry.setValue(value);
                 return;
             }
             entry.next = new Entry<>(key, value);
         }
+        this.size++;
     }
 
+    /**
+     * Gets value from the hashmap
+     * @param key key of the entry
+     * @return null if no entry is found by the specified key,
+     * and returns the value of the key is found
+     */
     public V get(K key){
         int hash = getHash(key) % SIZE;
         Entry<K, V> entry = table[hash];
@@ -50,7 +96,7 @@ public class CustomHashMap<K, V>{
         }
 
         while (entry != null) {
-            if (entry.getKey() == key) {
+            if (entry.getKey().equals(key)) {
                 return entry.getValue();
             }
             entry = entry.next;
@@ -58,15 +104,20 @@ public class CustomHashMap<K, V>{
         return null;
     }
 
+    /**
+     * Checks if the hashmap contains the desired key
+     * @param key the key to search for
+     * @return true if entry is found with the specified key, false otherwise
+     */
     public boolean containsKey(K key){
-        return !(this.get(key) == null);
+        return (get(key) != null);
 //        int hash = getHash(key) % SIZE;
 //        Entry<K, V> entry = table[hash];
 //        if (entry == null){
 //            return  false;
 //        }
 //        while (entry != null){
-//            if (entry.getKey() == key){
+//            if (entry.getKey().equals(key)){
 //                return true;
 //            }
 //            entry = entry.next;
@@ -74,13 +125,18 @@ public class CustomHashMap<K, V>{
 //        return false;
     }
 
+    /**
+     * Removes entry from the hashmap and returns the removed value
+     * @param key the key to look for in the hashmap
+     * @return null if no key is found, otherwise the removed entry
+     */
     public Entry<K, V> remove(K key){
         int hash = getHash(key) % SIZE;
         Entry<K, V> entry = table[hash];
         if (entry == null){
             return  null;
         }
-        if (entry.getKey() == key){
+        if (entry.getKey().equals(key)){
             table[hash] = entry.next;
             entry.next = null;
             return entry;
@@ -90,7 +146,7 @@ public class CustomHashMap<K, V>{
         entry = entry.next;
 
         while(entry != null){
-            if (entry.getKey() == key){
+            if (entry.getKey().equals(key)){
                 previous.next = entry.next;
                 entry.next = null;
                 return entry;
@@ -102,12 +158,16 @@ public class CustomHashMap<K, V>{
 
     }
 
-
-
+    /**
+     * LinkedList kind of class that will be an entry for the custom hashmap
+     * @param <K> Type of the key
+     * @param <V> Type of the value
+     */
     private class Entry<K, V>{
         private K key;
         private V value;
-        private Entry<K, V> next;
+        public Entry<K, V> next;
+
 
         public Entry(K key, V value){
             this.key = key;
@@ -125,6 +185,7 @@ public class CustomHashMap<K, V>{
         public void setValue(V value){
             this.value = value;
         }
-
     }
+
 }
+
